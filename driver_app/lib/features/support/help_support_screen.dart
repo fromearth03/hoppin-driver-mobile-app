3e2a2bff57a312d12453fb57e635449626843534
fromearth03 <aakbar.bese24seecs@seecs.edu.pk>
2026-08-19 01:06:54 +0500

    fix driver support contact imports

--- driver_app/lib/features/support/help_support_screen.dart
+++ driver_app/lib/features/support/help_support_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hoppin_shared/hoppin_shared.dart';
 import 'package:hoppin_ui/hoppin_ui.dart';
 
 import 'driver_support_categories.dart';
